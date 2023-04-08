362ea5e2dbb3e4a8f6af06e1bb9426a5329e3cec
Eric Van Vliet <eric@Erics-MacBook-Air.local>
2023-04-08 09:55:18 -0700

    pruned clipboard for hg

--- hg.py
+++ hg.py
@@ -4,43 +4,45 @@
 """ hg.py file
 
     clipboard contains screen of playing hangman.
-    extract g Guessed: string
-    extract w Word: string
-    construct s Answer: string
-    search dictionary for matches to s excluding g - w (exclusions)
-
+    extract Guessed: string
+    extract Word: string
+    construct suggested letter
+    search for matches to word excluding guessed letters
 """
 
 import re
 import string
 import collections
 from subprocess import call, check_output, Popen, PIPE
-import sys
 
+
+def ghs():
+    """
+    get last lines of clipboard containing hangman screen
+    """
+    s = check_output('pbpaste').decode('utf-8').split('\n')
+    k = len(s)
+    while k:
+        k = k - 1
+        if 'Guess' in s[k]:
+            return '\n'.join(s[k-12:k])
+    return ''
+ 
 class Hangman:  # guess for hangman
 
-    def __init__(self, fn):
-        screen = check_output('pbpaste').decode('utf-8')
+    def __init__(self):
         try:
-            reguess = r'Guessed: +([a-z]*)'
-            reword = r'Word: +([-a-z]*)'
-            self.guess = set(re.search(reguess, screen).group(1))
-            self.word = re.search(reword, screen).group(1)
-            self.length = len(self.word)
-            letters = set(self.word)
-            self.exclusions = self.guess - letters
-            intab = string.ascii_lowercase
-            outtab = "".join([l if l in letters else "-" for l in intab])
-            self.map = str.maketrans(intab, outtab)
+            screen = ghs()
             self.counter = collections.Counter()
+            self.guess = set(re.search(r'Guessed: +([a-z]*)', screen).group(1))
+            self.word = re.search(r'Word: +([-a-z]*)', screen).group(1)
+            self.length = len(self.word)
+            self.exclusions = self.guess - set(self.word)
+            self.map = str.maketrans(string.ascii_lowercase, "".join([l
+                if l in self.word else "-" for l in string.ascii_lowercase]))
         except:
             print("bad screen")
             quit()
-
-    def cch(self, word):
-        '''Maintain count of words per unguessed character.'''
-        self.counter.update([c for c in set(word) - self.guess])
-        return True
 
     def suggest(self):
         next_letter = self.counter.most_common(1)[0][0]
@@ -54,12 +56,12 @@ class Hangman:  # guess for hangman
         return (len(word) == self.length
             and not set(word) & self.exclusions
             and word.translate(self.map) == self.word
-            and self.cch(word))
+            or self.counter.update([c for c in set(word) - self.guess]))
 
 if __name__ == '__main__':
-    hangman = Hangman("hg")
+    hangman = Hangman()
     nm = 0
-    for line in open(sys.argv[1]):
+    for line in open("hg.dat"):
         if hangman.check(line.strip()):
             nm += 1
             if nm <= 8:
