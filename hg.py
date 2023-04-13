7009751593f0a32b6f99e9cf21bf7892958de8b7
Eric Van Vliet <eric@Erics-MacBook-Air.local>
2023-04-13 14:18:02 -0700

    separated data according to word length

--- hg.py
+++ hg.py
@@ -15,19 +15,6 @@ from collections import Counter
 from subprocess import check_output, Popen, PIPE
 
 
-def get_screen():
-    """
-    get lines from clipboard containing hangman screen
-    """
-    def last_word(s):
-        return s.split()[-1]
-
-    s = check_output('pbpaste').decode('utf-8').split('\n')
-    for k in range(len(s)-1, 0, -1):
-        if s[k][:5] == 'Guess':
-            return [last_word(s[k-1]), last_word(s[k-9])]
-
-
 def copy_char(c):
     """
     copy character to clipboard
@@ -35,45 +22,52 @@ def copy_char(c):
     Popen('pbcopy', stdin=PIPE).communicate(c)
 
 
+def get_screen():
+    """
+    get guessed and word from clipboard containing hangman screen
+    """
+    def last_word(s): return s.split()[-1]
+    s = check_output('pbpaste').decode('utf-8').split('\n')
+    for k in range(len(s)-1, 0, -1):
+        if s[k][:5] == 'Guess':
+            return (last_word(s[k-9]), last_word(s[k-1]))
+
+
 class Hangman:  # guess for hangman
 
-    def __init__(self):
-        try:
-            s = get_screen()
-            self.word = s[0]
-            self.guess = set(s[1])
-            self.length = len(self.word)
-            self.wrong = self.guess - set(self.word)
-            self.counter = Counter()
-            target = [l if l in self.word else "-" for l in ascii_lowercase]
-            self.map = str.maketrans(ascii_lowercase, "".join(target))
-        except:
-            print("bad screen")
-            quit()
+    def __init__(self, guess, word):
+        self.word = word
+        self.guess = set(guess)
+        self.wrong = self.guess - set(self.word)
+        self.data = f'hgdat/{len(self.word)}'
+        self.counter = Counter()
+        self.map = str.maketrans(ascii_lowercase, "".join(
+            [l if l in self.word else "-" for l in ascii_lowercase]))
 
     def suggest(self):
         '''get best guess'''
         return self.counter.most_common(1)[0][0].encode('utf-8')
 
     def check(self, word):
-        '''Check length, no bad guessed, matches guessed.'''
-        right = set(word)
-        if not right & self.wrong and word.translate(self.map) == self.word:
-            self.counter.update([c for c in right - self.guess])
-            print(word)
+        '''Check for wrong letters guessed and matches right ones.'''
+        if set(word) & self.wrong:
+            return False
+        if word.translate(self.map) == self.word:
+            self.counter.update([c for c in set(word) - self.guess])
             return True
         return False
 
 
 if __name__ == '__main__':
-    hangman = Hangman()
-    for line in open("hg.dat"):
-        word = line.strip()
-        if len(word) < hangman.length:
-            continue
-        if len(word) > hangman.length:
-            break
-        hangman.check(word)
+    try:
+        hangman = Hangman(*get_screen())
+        for line in open(hangman.data):
+            word = line.strip()
+            if hangman.check(word):
+                print(word)
+        # copy suggestion to clipboard
+        copy_char(hangman.suggest())
 
-    # copy suggestion to clipboard
-    copy_char(hangman.suggest())
+    except:
+        print('bad screen')
+        quit()
