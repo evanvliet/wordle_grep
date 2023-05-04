7121792b1ffb733bab775e35a01a9460ea3d2fbd
Eric Van Vliet <eric@Erics-MacBook-Air.local>
2023-05-04 10:03:59 -0700

    made python code more analagous to bash script

--- wordle_guess.py
+++ wordle_guess.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3 
 # 
+# A comand line wordle player, prints suggested word.
+#
+# wordle_guess.py <green letters> <yellow letters> <black letters>
+#
+# Uses python to find matches to a green, yellow, black specification. The
+# specification is three strings for the green, yellow, and black
+# characters on the last wordle puzzle line. Examples:
+#
+# <green letters>   e.g., ..a.b if 3rd and 5th letters are green a and b
+# <yellow letters>  e.g., .e.h  if 2nd and 4th letters are yellow e and h
+# <black letters>   e.g., jks   if j, k, and s all are black
+#
+# Use a dot (.) as a place holder, as position is significant for green
+# and yellow letters. A plain dot means no color character at that spot.
+# 
+# Saves matches to these arguments in a local file for the next guess.
+# 
+# Proceeds to pick a guess from this list. Looks for a word that has
+# the most characters in common with other possible matches.  Considers
+# most common letters among the matches first. For each character finds
+# matches with that letter. If some exist,  use the result for further
+# pruning. Prints the final smallest subset.
+# 
+# Called without arguments, it scans a word list from the pre-New York
+# Times version of wordle, saved as __file__.replace('.py', '.dat'), e.g.
+# wordle_guess.dat. In this case the above algorithm for picking a
+# guess suggests "arose" - an ok choice.
+
 import re
 import sys
 from collections import Counter
 
-FULL_LIST = "wordle_guess.dat"  # Full word list file
+FULL_LIST = __file__.replace('.py', '.dat')  # From old site
 WORD_LIST = "wg_last"  # Local file holding last matches
-
-def load_words(words):
-    try:
-        file = open(words, 'r')
-        return [word.strip() for word in file.readlines()]
-    except:
-        return None
 
 def mismatch(green, yellow, black):
     gyb = [] 
@@ -20,8 +41,8 @@ def mismatch(green, yellow, black):
     # because we can 'OR' patterns in an re but not 'AND' them
     # so we test NOT (not a or not b or not c or not d or not e)
     # to get a AND b AND c AND d AND e
-    gyb.append(f".*[{black}]") # match if any word black character
     gyb.append(f"(?!{green})") # mismatch green characters
+    gyb.append(f".*[{black}]") # match if any word black character
     dots='' # to specify position of yellow character
     for y in yellow:
         if y != '.':
@@ -30,35 +51,32 @@ def mismatch(green, yellow, black):
         dots=dots + '.' # add dot to position
     return re.compile("|".join(gyb))
 
-class WordleGuess:
-    def __init__(self, argv):
-        self.argv = argv[1:]
-        self.argv.extend(['.']*3)
-        self.words = load_words(WORD_LIST)
-        if not self.words or 1 == len(argv):
-            self.words = load_words(FULL_LIST)
-            
-    def match(self):
-        gyb_mis = mismatch(self.argv[0], self.argv[1], self.argv[2])
-        matches = [word for word in self.words if not gyb_mis.match(word)]
-        self.words = matches
+def load_words(wordlist):
+    try:
+        file = open(wordlist, 'r')
+        return [word.strip() for word in file.readlines()]
+    except:
+        return None
 
-    def save(self):
-        with open(WORD_LIST, 'w') as file:
-            file.write('\n'.join(self.words))
+# use full list if no args or no words from previous scan
+words = load_words(WORD_LIST)
+if not words or len(sys.argv) == 1:
+    words = load_words(FULL_LIST)
 
-    def guess(self):
-        letter_counts = Counter(''.join(self.words))
-        letters = [letter for letter, _ in letter_counts.most_common()]
-        for letter in letters:
-            subset = [word for word in self.words if letter in word]
-            if subset:
-                self.words = subset
-        return self.words[0] if self.words else ''
+# build regular expression to match green yellow black args
+argv = sys.argv[1:]
+argv.extend(['.']*3) # default "." values
+gyb_mis = mismatch(argv[0], argv[1], argv[2]) # get re
 
-if __name__ == "__main__":
-    # wg = WordleGuess("prog . ...s aroe".split()) #sys.argv)
-    wg = WordleGuess(sys.argv)
-    wg.match()
-    wg.save()
-    print(wg.guess())
+# get possible matches
+matches = [word for word in words if not gyb_mis.match(word)]
+open(WORD_LIST, 'w').write('\n'.join(matches)) # save for next run
+
+# pick guess from list, first get letters in order most frequent first
+letter_counts = Counter(''.join(matches))
+letters = [letter for letter, _ in letter_counts.most_common()]
+for letter in letters:
+    with_letters = [word for word in matches if letter in word]
+    if with_letters: # if letter has matches, use them as new subset
+        matches = with_letters
+print(matches[0] if matches else '') # suggested guess
